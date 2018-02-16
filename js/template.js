b2ab171000a2522a5cff19c9426058fdfd30bf45
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2018-02-16 23:20:53 +0100

    add template

---
+++ js/template.js
@@ -0,0 +1,11 @@
+/*
+ * template.js
+ * Copyright (C) 2018 nicolasfara <nicolas.farabegoli@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+$(document).ready(function(){
+  //Add below
+  $(".button-collapse").sideNav();
+});
