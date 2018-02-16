f9f61393f45f836fd654658cbfb92709dd0c85ce
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2018-02-17 00:48:45 +0100

    add jquery parallax

---
+++ js/parallax.js
@@ -0,0 +1,10 @@
+/*
+ * parallax.js
+ * Copyright (C) 2018 nicolasfara <nicolas.farabegoli@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
