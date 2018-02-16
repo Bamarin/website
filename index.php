8bd195d0f079d747195b87e8ccc53cf0766404c4
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2018-02-17 00:31:41 +0100

    Add jquery

--- index.php
+++ index.php
@@ -25,6 +25,7 @@
       include 'footer.php';
     ?>
     <!--JavaScript at end of body for optimized loading-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script type="text/javascript" src="js/materialize.min.js"></script>
     <script type="text/javascript" src="js/navbar.js"></script>
   </body>
