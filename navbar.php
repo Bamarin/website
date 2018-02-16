dd62f407f502d3daf1217edc3a031391965e00a9
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2018-02-16 23:45:58 +0100

    add footer & navbar

---
+++ navbar.php
@@ -0,0 +1,21 @@
+<?php
+echo '
+  <nav>
+    <div class="nav-wrapper light-blue darken-1">
+      <a href="#!" class="brand-logo">Bamarin [IT]</a>
+      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="#">About</a></li>
+        <li><a href="#">Models</a></li>
+        <li><a href="#">Donations</a></li>
+      </ul>
+      <ul class="side-nav" id="mobile-demo">
+        <li><a href="#">About</a></li>
+        <li><a href="#">Models</a></li>
+        <li><a href="#">Donations</a></li>
+      </ul>
+    </div>
+  </nav>'
+?>
+
+// vim: set filetype=eruby.html :
