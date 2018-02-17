6d5a02acd96efd21fac30b2d409a576bda95f88c
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2018-02-17 17:08:21 +0100

    fixed navbar and href to pages

--- navbar.php
+++ navbar.php
@@ -6,13 +6,13 @@ echo '
       <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
       <ul class="right hide-on-med-and-down">
         <li><a href="about.php">About</a></li>
-        <li><a href="#">Models</a></li>
-        <li><a href="#">Donations</a></li>
+        <li><a href="models.php">Models</a></li>
+        <li><a href="donations.php">Donations</a></li>
       </ul>
       <ul class="side-nav" id="mobile-demo">
         <li><a href="about.php">About</a></li>
-        <li><a href="#">Models</a></li>
-        <li><a href="#">Donations</a></li>
+        <li><a href="models.php">Models</a></li>
+        <li><a href="donations.php">Donations</a></li>
       </ul>
     </div>
   </nav>'
